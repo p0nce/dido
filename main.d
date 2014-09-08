c35617d3ab08987447436c4a8cfda04889b023ec
ponce <aliloko@gmail.com>
2014-09-09 00:22:10 +0200

    Do not search for dub.json if files are provided

--- main.d
+++ main.d
@@ -20,26 +20,29 @@ void main(string[] args)
         inputFiles ~= arg;
     }
 
-    if (exists("dub.json"))
-        inputFiles ~= "dub.json";
-    if (exists("package.json"))
-        inputFiles ~= "package.json";
-
-    auto dubResult = execute(["dub", "describe", "--nodeps"]);
-
-    if (dubResult.status != 0)
-        throw new Exception(format("dub returned %s", dubResult.status));
-
-    JSONValue description = parseJSON(dubResult.output);
-
-    foreach (pack; description["packages"].array())
+    if (inputFiles.length == 0)
     {
-        string packpath = pack["path"].str;
+        if (exists("dub.json"))
+            inputFiles ~= "dub.json";
+        if (exists("package.json"))
+            inputFiles ~= "package.json";
 
-        foreach (file; pack["files"].array())
+        auto dubResult = execute(["dub", "describe", "--nodeps"]);
+
+        if (dubResult.status != 0)
+            throw new Exception(format("dub returned %s", dubResult.status));
+
+        JSONValue description = parseJSON(dubResult.output);
+
+        foreach (pack; description["packages"].array())
         {
-            string filepath = file["path"].str();
-            inputFiles ~= buildPath(packpath, filepath);
+            string packpath = pack["path"].str;
+
+            foreach (file; pack["files"].array())
+            {
+                string filepath = file["path"].str();
+                inputFiles ~= buildPath(packpath, filepath);
+            }
         }
     }
 
