4ee7fe07052c5b2717601a0fca5b621005e251f1
ponce <aliloko@gmail.com>
2014-09-20 00:55:55 +0200

    Tweak scrollbar graphics.

--- dido/panel/scrollbar.d
+++ dido/panel/scrollbar.d
@@ -11,12 +11,15 @@ public:
     {
         super(context);
         _vertical = vertical;
+
+        _progressStart = 0.45f;
+        _progressStop = 0.55f;
     }
 
     override void reflow(box2i availableSpace)
     {
-        int width = 12;
-        int margin = 4;
+        int width = 10;
+        int margin = 2;
 
         _position = availableSpace;
 
@@ -38,8 +41,23 @@ public:
 
     override void preRender(SDL2Renderer renderer)
     {
-        renderer.setColor(34, 34, 34, 128);
+        renderer.setColor(34, 34, 34, 32);
         renderer.fillRect(0, 0, _position.width, _position.height);
+
+        
+        renderer.setColor(130, 130, 140, 32);
+        if (_vertical)
+        {
+            int iprogressStart = cast(int)(0.5f + _progressStart * _position.height);
+            int iprogressStop = cast(int)(0.5f + _progressStop * _position.height);
+            renderer.fillRect(1, iprogressStart, _position.width - 2, iprogressStop - iprogressStart);
+        }
+        else
+        {
+            int iprogressStart = cast(int)(0.5f + _progressStart * _position.width);
+            int iprogressStop = cast(int)(0.5f + _progressStop * _position.width);
+            renderer.fillRect(iprogressStart, 1, iprogressStop - iprogressStart, _position.height - 2);
+        }
     }
 
     void setState(float progressStart, float progressStop)
@@ -54,5 +72,6 @@ private:
     bool _vertical;
     float _progressStart;
     float _progressStop;
+    vec4i _color;
 
 }
