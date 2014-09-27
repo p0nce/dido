641061900c1a9437b2981f86e4e23169e0592892
ponce <aliloko@gmail.com>
2014-09-27 12:59:44 +0200

    Implements quote, if, set!, define

--- dido/script/package.d
+++ dido/script/package.d
@@ -13,8 +13,8 @@ import std.algorithm,
 
 alias Symbol = Typedef!string;
 
-// An atom is either a symbol, a symbol, or a list of atoms
-alias Atom = Algebraic!(string, Symbol, This[]);
+// An atom is either a string, a double, a symbol, or a list of atoms
+alias Atom = Algebraic!(string, double, Symbol, This[]);
 
 
 /// The one exception type thrown in this interpreter.
@@ -52,6 +52,15 @@ class Environment
             return outer.find(var);
         }
     }
+    ref Atom findSymbol(Symbol symbol)
+    {
+        string s = cast(string)symbol;
+        Environment env = find(s);
+        if (env is null)
+            throw new SchemeException(format("Variable '%d' is not defined", s));
+
+        return env.values[s];
+    }
 }
 
 __gshared Environment globalEnv;
@@ -79,20 +88,29 @@ shared static this()
     globalEnv = new Environment(defaultValues, null);
 }
 
+Atom makeNil()
+{
+    Atom[] values = [];
+    return Atom(values);
+}
 
-
-void eval(Atom atom, Environment env)
+Atom eval(Atom atom, Environment env)
 {
     Atom evalFailure(Atom x0)
     {
         throw new SchemeException(format("%s is not a function", toString(x0)));
     }
 
-    atom.visit!(
-        (Symbol sym) => env.find(cast(string)sym).values[cast(string)sym],
+    Atom result = atom.visit!(
+        (Symbol sym) => env.findSymbol(sym),
         (string s) => atom,
+        (double x) => atom,
         (Atom[] atoms)
         {
+            // empty list evaluate to itself
+            if (atoms.length == 0)
+                return atom;
+
             Atom x0 = atoms[0];
 
             return x0.visit!(
@@ -100,21 +118,80 @@ void eval(Atom atom, Environment env)
                 {
                     switch(cast(string)sym)
                     {
-                        case "quote":
+                        case "quote": 
+                            if (atoms.length < 2)
+                                throw new SchemeException("Empty quote");
+                            return Atom(atoms[1..$]);
+
                         case "if":
+                            if (atoms.length != 4)
+                                throw new SchemeException("Invalid if expression, should be (if test-expr then-expr else-expr)");
+                            if (toBool(eval(atoms[1], env)))
+                                return eval(atoms[2], env);
+                            else
+                                return eval(atoms[3], env);
+
                         case "set!":
+                            if (atoms.length != 3)
+                                throw new SchemeException("Invalid set! expression, should be (set! var exp)");
+                            env.findSymbol(atoms[1].toSymbol) = eval(atoms[2], env);
+                            return makeNil();
+
                         case "define":
+                            if (atoms.length != 3)
+                                throw new SchemeException("Invalid define expression, should be (define var exp)");
+                            env.values[cast(string)(toSymbol(atoms[1]))] = eval(atoms[2], env);
+                            return makeNil();
+
                         case "lambda":
                         case "begin":
                         default:
-                            return Atom("lol");
+/*
+                            // evaluate alls
+                            Atom[] evaluated;
+                            foreach(atom; atoms)  = atoms.map!eval(
+*/
+
+                            return makeNil();
                     }
                 },
                 (string s) => evalFailure(x0),
+                (double x) => evalFailure(x0),
                 (Atom[] atoms) => evalFailure(x0)
             );
         }
     );   
+    return result;
+}
+
+Symbol toSymbol(Atom atom)
+{
+    Symbol failure(Atom x0)
+    {
+        throw new SchemeException(format("%s is not a symbol", toString(x0)));
+    }
+
+    return atom.visit!(
+        (Symbol sym) => sym,
+        (string s) => failure(atom),
+        (double x) => failure(atom),
+        (Atom[] atoms) => failure(atom)
+    );
+}
+
+bool toBool(Atom atom)
+{
+    bool failure(Atom x0)
+    {
+        throw new SchemeException(format("%s cannot be converted to a truth value", toString(x0)));
+    }
+
+    return atom.visit!(
+        (Symbol sym) => failure(atom),
+        (string s) => s.length > 0, // "" is falsey
+        (double x) => x != 0, // 0 and NaN is falsey
+        (Atom[] atoms) => failure(atom) // empty list is falsey
+    );
 }
 
 string toString(Atom atom)
@@ -127,22 +204,13 @@ string toString(Atom atom)
     return atom.visit!(
         (Symbol sym) => cast(string)sym,
         (string s) => s,
+        (double x) => to!string(x),
         (Atom[] atoms) => atomJoiner(atoms)
     );
 }
+
 /+
-def eval(x, env=global_env):
-    "Evaluate an expression in an environment."
-    if isa(x, Symbol):             # variable reference
-        return env.find(x)[x]
-    elif not isa(x, list):         # constant literal
-        return x                
-    elif x[0] == 'quote':          # (quote exp)
-        (_, exp) = x
-        return exp
-    elif x[0] == 'if':             # (if test conseq alt)
-        (_, test, conseq, alt) = x
-        return eval((conseq if eval(test, env) else alt), env)
+
     elif x[0] == 'set!':           # (set! var exp)
         (_, var, exp) = x
         env.find(var)[var] = eval(exp, env)
