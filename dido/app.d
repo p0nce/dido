18fd894f303f02f091f1703cfe86ee235f804a34
ponce <aliloko@gmail.com>
2014-11-02 20:19:21 +0100

    Dido now consumes much less CPU.

--- dido/app.d
+++ dido/app.d
@@ -92,7 +92,7 @@ public:
             lastTime = time;
             _timeSinceKeypress += deltaTime;
 
-            pollCommandsFromKeyboard();
+            waitForAnEventThenProcessThemAll();
 
             SDL2Renderer renderer = _window.renderer();
 
@@ -154,129 +154,124 @@ private:
 
     bool _needReflow;
     bool _needRedraw;
-    
 
-    // retrieve list of commands to execute given by keyboard input
-    void pollCommandsFromKeyboard()
+    void dealWithEvent(ref SDL_Event event)
     {
-        SDL_Event event;
-        while (_sdl2.pollEvent(&event))
+        switch (event.type)
         {
-            switch (event.type)
-            {
-                case SDL_KEYDOWN:
+            case SDL_KEYDOWN:
+                {
+                    _timeSinceKeypress = 0;
+                    auto key = event.key.keysym;
+                    bool alt = (key.mod & KMOD_ALT) != 0;
+                    bool shift = (key.mod & KMOD_SHIFT) != 0;
+                    bool ctrl = (key.mod & KMOD_CTRL) != 0;
+                    string sshift = shift ? "#t" : "#f";
+
+                    if (key.sym == SDLK_RETURN && alt)
+                        _engine.executeScheme("(toggle-fullscreen)");
+                    else if (key.sym == SDLK_UP && ctrl && alt)
+                        _engine.executeScheme("(extend-selection-vertical -1)");
+                    else if (key.sym == SDLK_DOWN && ctrl && alt)
+                        _engine.executeScheme("(extend-selection-vertical 1)");
+                    else if (key.sym == SDLK_LEFT && ctrl)
+                        _engine.executeScheme(format("(move-word-left %s)", sshift));
+                    else if (key.sym == SDLK_RIGHT && ctrl)
+                        _engine.executeScheme(format("(move-word-right %s)", sshift));
+                    else if (key.sym == SDLK_ESCAPE)
+                        _engine.executeScheme("(escape)");
+                    else if (key.sym == SDLK_RETURN)
+                        _engine.enter();
+
+                    // copy/cut/paste
+                    else if (key.sym == SDLK_c && ctrl)
+                        _engine.executeScheme("(copy)");
+                    else if (key.sym == SDLK_x && ctrl)
+                        _engine.executeScheme("(cut)");
+                    else if (key.sym == SDLK_v && ctrl)
+                        _engine.executeScheme("(paste)");
+                    else if (key.sym == SDLK_COPY)
+                        _engine.executeScheme("(copy)");
+                    else if (key.sym == SDLK_CUT)
+                        _engine.executeScheme("(cut)");
+                    else if (key.sym == SDLK_PASTE)
+                        _engine.executeScheme("(paste)");
+                    else if (key.sym == SDLK_DELETE && shift)
+                        _engine.executeScheme("(cut)");
+                    else if (key.sym == SDLK_INSERT && ctrl)
+                        _engine.executeScheme("(copy)");
+                    else if (key.sym == SDLK_INSERT && shift)
+                        _engine.executeScheme("(paste)");
+                    else if (key.sym == SDLK_LEFT)
+                        _engine.executeScheme(format("(move-horizontal -1 %s)", sshift));
+                    else if (key.sym == SDLK_RIGHT)
+                        _engine.executeScheme(format("(move-horizontal 1 %s)", sshift));
+                    else if (key.sym == SDLK_UP)
+                        _engine.executeScheme(format("(move-vertical -1 %s)", sshift));
+                    else if (key.sym == SDLK_DOWN)
+                        _engine.executeScheme(format("(move-vertical 1 %s)", sshift));
+                    else if (key.sym == SDLK_BACKSPACE)
+                        _engine.executeScheme("(delete-selection #t)");
+                    else if (key.sym == SDLK_DELETE)
+                        _engine.executeScheme("(delete-selection #f)");
+                    else if (key.sym == SDLK_HOME && ctrl)
+                        _engine.executeScheme(format("(move-buffer-start %s)", sshift));
+                    else if (key.sym == SDLK_END && ctrl)
+                        _engine.executeScheme(format("(move-buffer-end %s)", sshift));
+                    else if (key.sym == SDLK_a && ctrl)
+                        _engine.executeScheme("(select-all)");
+                    else if (key.sym == SDLK_END)
+                        _engine.executeScheme(format("(move-line-end %s)", sshift));
+                    else if (key.sym == SDLK_HOME)
+                        _engine.executeScheme(format("(move-line-start %s)", sshift));
+                    else if (key.sym == SDLK_PAGEUP && ctrl)
+                        _engine.executeScheme("(previous-buffer)");
+                    else if (key.sym == SDLK_PAGEDOWN && ctrl)
+                        _engine.executeScheme("(next-buffer)");
+                    else if (key.sym == SDLK_PAGEUP)
+                        _engine.executeScheme(format("(move-vertical (- (visible-lines)) %s)", sshift));
+                    else if (key.sym == SDLK_PAGEDOWN)
+                        _engine.executeScheme(format("(move-vertical (visible-lines) %s)", sshift));
+                    else if (key.sym == SDLK_z && ctrl)
+                        _engine.executeScheme("(undo)");
+                    else if (key.sym == SDLK_y && ctrl)
+                        _engine.executeScheme("(redo)");
+                    else if (key.sym == SDLK_s && ctrl)
+                        _engine.executeScheme("(save)");
+                    else if (key.sym == SDLK_F4)
+                        _engine.executeScheme("(build)");
+                    else if (key.sym == SDLK_F5)
+                        _engine.executeScheme("(run)");
+                    else if (key.sym == SDLK_TAB)
+                        _engine.executeScheme("(indent)");
+                    else 
                     {
-                        _timeSinceKeypress = 0;
-                        auto key = event.key.keysym;
-                        bool alt = (key.mod & KMOD_ALT) != 0;
-                        bool shift = (key.mod & KMOD_SHIFT) != 0;
-                        bool ctrl = (key.mod & KMOD_CTRL) != 0;
-                        string sshift = shift ? "#t" : "#f";
-
-                        if (key.sym == SDLK_RETURN && alt)
-                            _engine.executeScheme("(toggle-fullscreen)");
-                        else if (key.sym == SDLK_UP && ctrl && alt)
-                            _engine.executeScheme("(extend-selection-vertical -1)");
-                        else if (key.sym == SDLK_DOWN && ctrl && alt)
-                            _engine.executeScheme("(extend-selection-vertical 1)");
-                        else if (key.sym == SDLK_LEFT && ctrl)
-                            _engine.executeScheme(format("(move-word-left %s)", sshift));
-                        else if (key.sym == SDLK_RIGHT && ctrl)
-                            _engine.executeScheme(format("(move-word-right %s)", sshift));
-                        else if (key.sym == SDLK_ESCAPE)
-                            _engine.executeScheme("(escape)");
-                        else if (key.sym == SDLK_RETURN)
-                            _engine.enter();
-
-                        // copy/cut/paste
-                        else if (key.sym == SDLK_c && ctrl)
-                            _engine.executeScheme("(copy)");
-                        else if (key.sym == SDLK_x && ctrl)
-                            _engine.executeScheme("(cut)");
-                        else if (key.sym == SDLK_v && ctrl)
-                            _engine.executeScheme("(paste)");
-                        else if (key.sym == SDLK_COPY)
-                            _engine.executeScheme("(copy)");
-                        else if (key.sym == SDLK_CUT)
-                            _engine.executeScheme("(cut)");
-                        else if (key.sym == SDLK_PASTE)
-                            _engine.executeScheme("(paste)");
-                        else if (key.sym == SDLK_DELETE && shift)
-                            _engine.executeScheme("(cut)");
-                        else if (key.sym == SDLK_INSERT && ctrl)
-                            _engine.executeScheme("(copy)");
-                        else if (key.sym == SDLK_INSERT && shift)
-                            _engine.executeScheme("(paste)");
-                        else if (key.sym == SDLK_LEFT)
-                            _engine.executeScheme(format("(move-horizontal -1 %s)", sshift));
-                        else if (key.sym == SDLK_RIGHT)
-                            _engine.executeScheme(format("(move-horizontal 1 %s)", sshift));
-                        else if (key.sym == SDLK_UP)
-                            _engine.executeScheme(format("(move-vertical -1 %s)", sshift));
-                        else if (key.sym == SDLK_DOWN)
-                            _engine.executeScheme(format("(move-vertical 1 %s)", sshift));
-                        else if (key.sym == SDLK_BACKSPACE)
-                            _engine.executeScheme("(delete-selection #t)");
-                        else if (key.sym == SDLK_DELETE)
-                            _engine.executeScheme("(delete-selection #f)");
-                        else if (key.sym == SDLK_HOME && ctrl)
-                            _engine.executeScheme(format("(move-buffer-start %s)", sshift));
-                        else if (key.sym == SDLK_END && ctrl)
-                            _engine.executeScheme(format("(move-buffer-end %s)", sshift));
-                        else if (key.sym == SDLK_a && ctrl)
-                            _engine.executeScheme("(select-all)");
-                        else if (key.sym == SDLK_END)
-                            _engine.executeScheme(format("(move-line-end %s)", sshift));
-                        else if (key.sym == SDLK_HOME)
-                            _engine.executeScheme(format("(move-line-start %s)", sshift));
-                        else if (key.sym == SDLK_PAGEUP && ctrl)
-                            _engine.executeScheme("(previous-buffer)");
-                        else if (key.sym == SDLK_PAGEDOWN && ctrl)
-                            _engine.executeScheme("(next-buffer)");
-                        else if (key.sym == SDLK_PAGEUP)
-                            _engine.executeScheme(format("(move-vertical (- (visible-lines)) %s)", sshift));
-                        else if (key.sym == SDLK_PAGEDOWN)
-                            _engine.executeScheme(format("(move-vertical (visible-lines) %s)", sshift));
-                        else if (key.sym == SDLK_z && ctrl)
-                            _engine.executeScheme("(undo)");
-                        else if (key.sym == SDLK_y && ctrl)
-                            _engine.executeScheme("(redo)");
-                        else if (key.sym == SDLK_s && ctrl)
-                            _engine.executeScheme("(save)");
-                        else if (key.sym == SDLK_F4)
-                            _engine.executeScheme("(build)");
-                        else if (key.sym == SDLK_F5)
-                            _engine.executeScheme("(run)");
-                        else if (key.sym == SDLK_TAB)
-                            _engine.executeScheme("(indent)");
-                        else 
-                        {
-                        }
-
-                        _needReflow = true;
-                        _needRedraw = true;
-                        break;
                     }
 
-                case SDL_TEXTINPUT:
-                    {
-                        _timeSinceKeypress = 0;
-                        string s = sanitizeUTF8(event.text.text.ptr);
-
-                        if (s == ":")
-                            _engine.enterCommandLineMode();
-                        else
-                        {
-                            dstring ds = to!dstring(s);
-                            foreach(ch; ds)
-                                _engine.executeScheme(format("(insert-char %s)", to!int(ch)));
-                        }
-                    }
+                    _needReflow = true;
                     _needRedraw = true;
                     break;
+                }
+
+            case SDL_TEXTINPUT:
+                {
+                    _timeSinceKeypress = 0;
+                    string s = sanitizeUTF8(event.text.text.ptr);
+
+                    if (s == ":")
+                        _engine.enterCommandLineMode();
+                    else
+                    {
+                        dstring ds = to!dstring(s);
+                        foreach(ch; ds)
+                            _engine.executeScheme(format("(insert-char %s)", to!int(ch)));
+                    }
+                }
+                _needRedraw = true;
+                break;
 
 
-                case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONDOWN:
                 {
                     const (SDL_MouseButtonEvent*) mbEvent = &event.button;
 
@@ -291,7 +286,7 @@ private:
                     break;
                 }
 
-                case SDL_MOUSEBUTTONUP:
+            case SDL_MOUSEBUTTONUP:
                 {
                     const (SDL_MouseButtonEvent*) mbEvent = &event.button;
                     _mainPanel.mouseRelease(_sdl2.mouse.x, _sdl2.mouse.y, mbEvent.button);
@@ -299,26 +294,38 @@ private:
                     break;
                 }
 
-                case SDL_MOUSEWHEEL:
+            case SDL_MOUSEWHEEL:
                 {
                     _mainPanel.mouseWheel(_sdl2.mouse.x, _sdl2.mouse.y, _sdl2.mouse.wheelDeltaX(), _sdl2.mouse.wheelDeltaY());
                     _needRedraw = true;
                     break;
                 }
 
-                case SDL_MOUSEMOTION:
-                    _mainPanel.mouseMove(_sdl2.mouse.x, _sdl2.mouse.y, _sdl2.mouse.lastDeltaX(), _sdl2.mouse.lastDeltaY());
-                    _needRedraw = true;
-                    break;
+            case SDL_MOUSEMOTION:
+                _mainPanel.mouseMove(_sdl2.mouse.x, _sdl2.mouse.y, _sdl2.mouse.lastDeltaX(), _sdl2.mouse.lastDeltaY());
+                _needRedraw = true;
+                break;
 
-                case SDL_WINDOWEVENT:
-                    _needReflow = true;
-                    _needRedraw = true;
-                    break;
+            case SDL_WINDOWEVENT:
+                _needReflow = true;
+                _needRedraw = true;
+                break;
 
 
-                default:
-                    break;
+            default:
+                break;
+        }
+    }
+    
+    void waitForAnEventThenProcessThemAll()
+    {
+        SDL_Event event;
+        if (_sdl2.waitEventTimeout(&event, 200))
+        {
+            dealWithEvent(event);
+            while (_sdl2.pollEvent(&event))
+            {
+                dealWithEvent(event);
             }
         }
     }
